920072b78afe2c43c6a503e99209e4b9c455f92c
alvina-dr <alvinadr24@gmail.com>
2025-03-17 11:32:14 +0100

    init commit

---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,41 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public CharacterController CharacterController;
+    public CinemachineCamera CinemachineCamera;
+    private Vector3 _moveDirection;
+    [SerializeField]
+    private float _moveSpeed;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        transform.forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _moveDirection = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        //transform.forward = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotatedDirection = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * new Vector3(_moveDirection.x, 0, _moveDirection.y);
+
+        //Vector3 vector = new Vector3(_moveDirection.x * transform.forward.x, 0, _moveDirection.y * transform.forward.z);
+        CharacterController.Move(rotatedDirection * _moveSpeed);
+    }
+}
